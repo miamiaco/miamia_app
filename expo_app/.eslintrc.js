60aa74ba5242e9c998f792e12a3837394ce312fa
Ronja Kovero <ronja.kovero@gmail.com>
2024-05-23 17:03:06 +0300

    added typescrip for eslint

--- expo_app/.eslintrc.js
+++ expo_app/.eslintrc.js
@@ -1,4 +1,21 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'expo',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020, 
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
