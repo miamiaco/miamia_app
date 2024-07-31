186ad48f29a829e6849311c94aeeb0d5f4946f9d
Vanessa Pihlaja <vanessa.pihlaja@aalto.fi>
2024-07-31 13:35:56 +0300

    Added navbar, search and profile pages, and updated feedscreen

--- expo_app/src/App.tsx
+++ expo_app/src/App.tsx
@@ -1,9 +1,16 @@
 import React, { useCallback } from 'react';
 import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import * as SplashScreen from 'expo-splash-screen';
 import { useFonts } from 'expo-font';
+import FacebookLoginScreen from './screens/FacebookLoginScreen/FacebookLoginScreen';
+import FeedScreen from './screens/FeedScreen/FeedScreen';
+import ProfileScreen from './screens/ProfileScreen/ProfileScreen';
+import SearchScreen from './screens/SearchScreen/SearchScreen';
+import Header from './components/Header/Header';
 import {
   Outfit_100Thin,
   Outfit_200ExtraLight,
@@ -16,13 +23,10 @@ import {
   Outfit_900Black,
 } from '@expo-google-fonts/outfit';
 
-import FacebookLoginScreen from './screens/FacebookLoginScreen/FacebookLoginScreen';
-import FeedScreen from './screens/FeedScreen/FeedScreen';
-import Header from './components/Header/Header';
-
 SplashScreen.preventAutoHideAsync();
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App: React.FC = () => {
   const [fontsLoaded] = useFonts({
@@ -50,16 +54,38 @@ const App: React.FC = () => {
   return (
     <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="FeedScreen">
-          <Stack.Screen 
+        <Tab.Navigator>
+          <Tab.Screen 
             name="Feed" 
             component={FeedScreen} 
             options={{
-              header: () => <Header />
+              header: () => <Header />,
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="home" />
+              )
             }} 
           />
-          <Stack.Screen name="FeedScreen" component={FeedScreen} />
-        </Stack.Navigator>
+          <Tab.Screen 
+            name="Search" 
+            component={SearchScreen} 
+            options={{
+              header: () => <Header />,
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="Search" />
+              )
+            }} 
+          />
+          <Tab.Screen 
+            name="Profile" 
+            component={ProfileScreen} 
+            options={{
+              header: () => <Header />,
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="Profile" />
+              )
+            }} 
+          />
+        </Tab.Navigator>
       </NavigationContainer>
     </View>
   );
