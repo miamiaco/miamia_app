560e632b2d692baf5a50c637e2d8d615ae699e7a
Ronja Kovero <ronja.kovero@gmail.com>
2024-07-31 14:39:59 +0300

    eas build configurations

--- expo_app/app.config.ts
+++ expo_app/app.config.ts
@@ -12,5 +12,8 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     supabaseUrl: process.env.SUPABASE_URL,
     supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
     facebookAppId: process.env.FACEBOOK_APP_ID,
+    eas: {
+        "projectId": "c59934c7-494c-4d74-bc9a-edac7dd6013b"
+      }
   },
 });
